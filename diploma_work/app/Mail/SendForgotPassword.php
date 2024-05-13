f353f2fff98a01d96977af395e3726060f5106bc
Vazhd <noise123457@gmail.com>
2024-05-13 14:58:16 +0500

    auth refactoring

--- diploma_work/app/Mail/SendForgotPassword.php
+++ diploma_work/app/Mail/SendForgotPassword.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PasswordResetMail extends Mailable
+class SendForgotPassword extends Mailable
 {
     use Queueable, SerializesModels;
 
