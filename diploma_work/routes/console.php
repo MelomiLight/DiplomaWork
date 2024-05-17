2504f3f00220afb95e635f61012fe3b5ac043a18
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2024-05-17 19:12:22 +0500

    cron added

--- diploma_work/routes/console.php
+++ diploma_work/routes/console.php
@@ -1,12 +1,25 @@
 <?php
 
-use App\Jobs\SendMessage;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    while (true){
-        SendMessage::dispatch();
-    }
-})->purpose('Display an inspiring quote')->hourly();
+// Initial setup
+use App\Services\ScheduleService;
+use Illuminate\Support\Facades\Schedule;
 
+Schedule::call(function (ScheduleService $scheduleService) {
+    $scheduleService->initializeUsers();
+})->cron('* * * * *'); // This can be any time for the initial setup
+
+// Daily reset
+Schedule::call(function (ScheduleService $scheduleService) {
+    $scheduleService->updateDailyChallenges();
+})->cron('0 0 * * *'); // Daily reset at midnight
+
+// Weekly reset
+Schedule::call(function (ScheduleService $scheduleService) {
+    $scheduleService->updateWeeklyChallenges();
+})->cron('0 0 * * 1'); // Weekly reset on Monday at midnight
+
+// Monthly reset
+Schedule::call(function (ScheduleService $scheduleService) {
+    $scheduleService->updateMonthlyChallenges();
+})->cron('0 0 1 * *'); // Monthly reset on the 1st day at midnight
