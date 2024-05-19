bb69aee7e095b3e098780602f2da023cbfd29830
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2024-05-19 05:03:05 +0500

    cc

--- diploma_work/app/Services/ChallengeService.php
+++ diploma_work/app/Services/ChallengeService.php
@@ -6,15 +6,24 @@ use App\Http\Requests\ChallengeRequest;
 use App\Models\Challenge;
 use App\Models\User;
 use App\Models\UserChallenge;
+use App\Models\UserPoint;
 use Illuminate\Support\Facades\DB;
 
 class ChallengeService
 {
-    public function checkDaily(User $user, UserChallenge $userChallenge): bool
+    public function checkCondition(User $user, UserChallenge $userChallenge, $distance): bool
     {
-        if ($user->runInformation->daily_distance_km >= $userChallenge->challenge->distance_km) {
+        if ($distance >= $userChallenge->challenge->distance_km) {
             $userChallenge->challenge_status = true;
             $user->points += $userChallenge->challenge->points;
+
+            UserPoint::create([
+                'user_id' => $user->id,
+                'prev_points' => $user->points,
+                'earned_points' => $userChallenge->challenge->points,
+                'earned_date' => now(),
+            ]);
+
             $user->save();
             $userChallenge->save();
 
@@ -22,34 +31,21 @@ class ChallengeService
         }
 
         return false;
+    }
+
+    public function checkDaily(User $user, UserChallenge $userChallenge): bool
+    {
+        return $this->checkCondition($user, $userChallenge, $user->runInformation->daily_distance_km);
     }
 
     public function checkWeekly(User $user, UserChallenge $userChallenge): bool
     {
-        if ($user->runInformation->weekly_distance_km >= $userChallenge->challenge->distance_km) {
-            $userChallenge->challenge_status = true;
-            $user->points += $userChallenge->challenge->points;
-            $user->save();
-            $userChallenge->save();
-
-            return true;
-        }
-
-        return false;
+        return $this->checkCondition($user, $userChallenge, $user->runInformation->weekly_distance_km);
     }
 
     public function checkMonthly(User $user, UserChallenge $userChallenge): bool
     {
-        if ($user->runInformation->monthly_distance_km >= $userChallenge->challenge->distance_km) {
-            $userChallenge->challenge_status = true;
-            $user->points += $userChallenge->challenge->points;
-            $user->save();
-            $userChallenge->save();
-
-            return true;
-        }
-
-        return false;
+        return $this->checkCondition($user, $userChallenge, $user->runInformation->monthly_distance_km);
     }
 
     public function create(ChallengeRequest $request)
