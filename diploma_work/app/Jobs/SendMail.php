bc7f81e7c5bc18f1ba5ece14973dc2e8401a368c
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2024-05-20 17:56:09 +0500

    FEAT mail send through jobs,queues

---
+++ diploma_work/app/Jobs/SendMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\SendForgotPassword;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected User $user;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->user->email)->send(new SendForgotPassword($this->user->reset_code));
+    }
+}
