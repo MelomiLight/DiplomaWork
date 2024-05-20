0805af70ee4994d9b6820826f7308aecf268f51a
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2024-05-20 18:26:20 +0500

    bug fixed

--- diploma_work/app/Http/Requests/Auth/RegisterRequest.php
+++ diploma_work/app/Http/Requests/Auth/RegisterRequest.php
@@ -23,7 +23,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users|bail'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'bail'],
             'password' => ['required', 'string', 'min:8'],
         ];
     }
