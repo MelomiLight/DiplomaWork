9a47fdcfba7a0c50ef07535af4ca4e69f1149c93
Vazhd <noise123457@gmail.com>
2024-05-24 12:30:20 +0500

    Feat: swagger for auth added

--- diploma_work/app/Http/Controllers/Auth/AuthController.php
+++ diploma_work/app/Http/Controllers/Auth/AuthController.php
@@ -26,7 +26,46 @@ class AuthController extends Controller
     }
 
     /**
+     * Get a JWT via given credentials.
+     *
+     * @param RegisterRequest $request
+     * @return JsonResponse
+     *
      * @throws Exception
+     * @OA\Post(
+     *      path="/api/register",
+     *      summary="Регистрация",
+     *      tags={"Авторизация"},
+     *
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json"
+     *      ),
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"name","email","password"},
+     *              @OA\Property(property="name", type="string", example="user"),
+     *              @OA\Property(property="email", type="string", example="user230@crocos.kz"),
+     *              @OA\Property(property="password", type="string", example="12345678"),
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="token", type="string", example="your_token"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *         response=500,
+     *         description="Could not create user"
+     *      ),
+     * )
      */
     public function register(RegisterRequest $request): JsonResponse
     {
@@ -37,7 +76,50 @@ class AuthController extends Controller
 
 
     /**
+     * Get a JWT via given credentials.
+     *
+     * @param LoginRequest $request
+     * @return UserResource|JsonResponse
+     *
      * @throws Exception
+     * @OA\Post(
+     *      path="/api/login",
+     *      summary="Логин",
+     *      tags={"Авторизация"},
+     *
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json"
+     *      ),
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"email","password"},
+     *              @OA\Property(property="email", type="string", example="user230@crocos.kz"),
+     *              @OA\Property(property="password", type="string", example="12345678"),
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="array", @OA\Items(type="string", example="user_info"))),
+     *              @OA\Property(property="token", type="string", example="your_token"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *         response=401,
+     *         description="These credentials do not match our records."
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Could not create token"
+     *       ),
+     * )
      */
     public function login(LoginRequest $request): UserResource|JsonResponse
     {
@@ -53,7 +135,40 @@ class AuthController extends Controller
     }
 
     /**
+     *
+     * @param ForgotRequest $request
+     * @return JsonResponse
+     *
      * @throws Exception
+     * @OA\Post(
+     *      path="/api/forgot",
+     *      summary="Забыл пароль",
+     *      tags={"Авторизация"},
+     *
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json"
+     *      ),
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"email"},
+     *              @OA\Property(property="email", type="string", example="user230@crocos.kz"),
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="We have emailed your password reset link.",
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Could not send mail or could not create reset code"
+     *       ),
+     * )
      */
     public function forgotPassword(ForgotRequest $request): JsonResponse
     {
@@ -65,7 +180,42 @@ class AuthController extends Controller
     }
 
     /**
+     *
+     * @param ChangeRequest $request
+     * @return JsonResponse
+     *
      * @throws Exception
+     * @OA\Post(
+     *      path="/api/password/reset",
+     *      summary="Поменять пароль",
+     *      tags={"Авторизация"},
+     *
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json"
+     *      ),
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"email", "password", "reset_code"},
+     *              @OA\Property(property="email", type="string", example="user230@crocos.kz"),
+     *              @OA\Property(property="password", type="string", example="12345678"),
+     *              @OA\Property(property="reset_code", type="string", example="f28adw"),
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Your password has been reset.",
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Could not change password"
+     *       ),
+     * )
      */
     public function changePassword(ChangeRequest $request): JsonResponse
     {
@@ -74,6 +224,39 @@ class AuthController extends Controller
         return response()->json(['message' => __('messages.reset')]);
     }
 
+    /**
+     *
+     * @return JsonResponse
+     *
+     * @OA\Post(
+     *      path="/api/logout",
+     *      summary="Поменять пароль",
+     *      tags={"Авторизация"},
+     *
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json"
+     *      ),
+     *       @OA\Parameter(
+     *          name="Authorization",
+     *          in="header",
+     *          required=true,
+     *          example="Bearer your_token"
+     *       ),
+     *
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successfully logged out",
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="error message"
+     *       ),
+     * )
+     */
     public function logout(): JsonResponse
     {
         try {
