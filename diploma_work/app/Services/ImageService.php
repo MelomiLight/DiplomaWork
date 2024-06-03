1762bba7c94e17d569b2a3f42308ab56eff420bb
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2024-06-03 10:21:33 +0500

    Fix: api routes, token->access_token, b64 to form-data
    Signed-off-by: MelomiLight <97735001+MelomiLight@users.noreply.github.com>

--- diploma_work/app/Services/ImageService.php
+++ diploma_work/app/Services/ImageService.php
@@ -12,51 +12,14 @@ class ImageService
     /**
      * @throws Exception
      */
-    public function store($imageData, $path = 'images'): string
+    public function store($request, $path = 'images'): string
     {
-        try {
-            $imageName = $this->saveBase64Image($imageData, $path);
-        } catch (Exception $e) {
-            __('message');
-            throw new \Exception($e->getMessage());
-        }
+        $image = $request->file('profile_picture');
 
-        return $imageName;
-    }
+        $imageName = time() . '.' . $image->extension();
 
-    private function saveBase64Image($base64Image, $path): string
-    {
-        // Extract the file extension from the base64 string
-        preg_match('/data:image\/(.*?);base64,/', $base64Image, $matches);
-        $extension = $matches[1];
+        $image->storeAs($path, $imageName);
 
-        // Remove the base64 header
-        $image = preg_replace('/^data:image\/(.*?);base64,/', '', $base64Image);
-        $image = str_replace(' ', '+', $image);
-
-        // Generate a unique filename with the correct extension
-        $imageName = $path . '/' . uniqid() . '.' . $extension;
-
-        // Save the image to the storage
-        Storage::disk('public')->put($imageName, base64_decode($image));
-
-        return $imageName;
-    }
-
-    /**
-     * @throws Exception
-     */
-    public static function getBase64Image($path): string
-    {
-        if ($path) {
-            if (Storage::disk('public')->exists($path)) {
-                $file = Storage::disk('public')->get($path);
-                $type = Storage::disk('public')->mimeType($path);
-                // Encode the image back to base64
-                return 'data:' . $type . ';base64,' . base64_encode($file);
-            }
-        }
-        return '';
-
+        return $path . '/' . $imageName;
     }
 }
--- diploma_work/routes/api.php
+++ diploma_work/routes/api.php
@@ -9,10 +9,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('forgot', [AuthController::class, 'forgotPassword']);
@@ -23,30 +19,30 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::prefix('/run')->group(function () {
-        Route::post('/session/store', [RunningSessionController::class, 'store']);
-        Route::get('/session/index', [RunningSessionController::class, 'index']);
-        Route::delete('/session/delete/{runningSession}', [RunningSessionController::class, 'destroy']);
+        Route::post('/session', [RunningSessionController::class, 'store']);
+        Route::get('/session', [RunningSessionController::class, 'index']);
+        Route::delete('/session/{runningSession}', [RunningSessionController::class, 'destroy']);
     });
 
     Route::prefix('/user')->group(function () {
-        Route::get('/index', [UserController::class, 'index']);
-        Route::get('/show/{user}', [UserController::class, 'show']);
-        Route::patch('/update', [UserController::class, 'update']);
-        Route::delete('/delete/{user}', [UserController::class, 'destroy']);
+        Route::get('/', [UserController::class, 'index']);
+        Route::get('/{user}', [UserController::class, 'show']);
+        Route::patch('/', [UserController::class, 'update']);
+        Route::delete('/{user}', [UserController::class, 'destroy']);
 
         Route::prefix('/challenges')->group(function () {
-            Route::post('/store', [UserChallengeController::class, 'store']);
-            Route::get('/show', [UserChallengeController::class, 'show']);
-            Route::delete('/delete', [UserChallengeController::class, 'destroy']);
+            Route::post('/', [UserChallengeController::class, 'store']);
+            Route::get('/', [UserChallengeController::class, 'show']);
+            Route::delete('/', [UserChallengeController::class, 'destroy']);
         });
 
     });
 
     Route::prefix('/challenges')->group(function () {
         Route::get('/check', [CheckChallengesController::class, 'setChallenge']);
-        Route::post('/store', [CheckChallengesController::class, 'store']);
-        Route::get('/index', [CheckChallengesController::class, 'index']);
-        Route::delete('/delete/{challenge}', [CheckChallengesController::class, 'destroy']);
+        Route::post('/', [CheckChallengesController::class, 'store']);
+        Route::get('/', [CheckChallengesController::class, 'index']);
+        Route::delete('/{challenge}', [CheckChallengesController::class, 'destroy']);
     });
 
     Route::get('/leaderboard', [LeaderboardController::class, 'index']);
