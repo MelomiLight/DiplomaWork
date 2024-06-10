b9c7049211c637c0af84be4a6a93b8a67ea09baa
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2024-06-10 12:57:37 +0500

    fix

--- diploma_work/app/Models/RunningSession.php
+++ diploma_work/app/Models/RunningSession.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -27,6 +28,11 @@ class RunningSession extends Model
         'start_time' => 'datetime:H:i:s',
         'end_time' => 'datetime:H:i:s',
     ];
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 
     /**
      * Get the user that owns the running session.
